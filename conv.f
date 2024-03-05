ab86f90a8ce1dc31c4d97927321b1e1cecd2ba81
chibaf <chibaf@mac.com>
2024-03-05 14:14:28 +0900

    Add files via upload

---
+++ conv.f
@@ -0,0 +1,14 @@
+      real*4 d(11),dd
+      do while (2>1)
+        read(*,*,end=200) (d(i),i=1,11)
+        dd=0.0
+        do i=2,11
+          dd=dd+abs(d(i))
+        end do
+        if (dd /= 0.0) then
+          write(*,100)(d(i),i=1,11)
+        end if
+      end do
+  100 format(F9.2,10F8.4)
+  200 stop
+      end
